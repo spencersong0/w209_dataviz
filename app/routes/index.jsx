b5127826c4f07ac41e8fe696023a61cd0ac1a93e
Kklu78 <76934184+Kklu78@users.noreply.github.com>
2022-05-31 21:24:11 -0700

    update

---
+++ app/routes/index.jsx
@@ -0,0 +1,10 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4"}}>
+      <div style={{display: 'flex', justifyContent: 'center'}}><h1>Tableau VIS</h1></div>
+      
+      <iframe src="https://public.tableau.com/views/W209HW4_16532659392950/Sheet1?:showVizHome=no:embed=true"
+ width="2000" height="955"></iframe>
+    </div>
+  );
+}
