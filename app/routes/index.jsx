88b42f9e8b3b2b932372294f60960432200f90f2
Kklu78 <76934184+Kklu78@users.noreply.github.com>
2022-07-13 17:38:21 -0700

    update

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -4,29 +4,26 @@ export default function Index() {
       className="flex flex-col items-center max-w-fit"
       style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}
     >
-      <h1 className="text-3xl">W209 Data VIS</h1>
-
+      <h1> Temporal View</h1>
       <iframe
-        src="https://public.tableau.com/views/W209HW4_16532659392950/Sheet1?:showVizHome=no:embed=true"
-        width="1800"
-        height="955"
+        src="https://public.tableau.com/views/TemporalViewofAccidents/TemporalView?:showVizHome=no:embed=true"
+        width="1400"
+        height="875"
       ></iframe>
-      <br></br>
 
-      <p className=''>
-        Pellentesque habitant morbi tristique senectus et netus et malesuada
-        fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
-        ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
-        egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend
-        leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum
-        erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean
-        fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci,
-        sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar
-        facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor
-        neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat
-        volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis
-        luctus, metus
-      </p>
+      <h1>Weather View</h1>
+      <iframe
+        src="https://public.tableau.com/views/TrafficAccidentsWeather/Dashboard1?:showVizHome=no:embed=true"
+        width="1400"
+        height="875"
+      ></iframe>
+
+      <h1>Road Type View</h1>
+      <iframe
+        src="https://public.tableau.com/views/W209FinalProject-AllData/Dashboard1?:showVizHome=no:embed=true"
+        width="1400"
+        height="875"
+      ></iframe>
     </div>
   );
 }
